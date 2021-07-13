c8d8e2b7451d5ee6c94dcb3fe4706d3847dae4e9
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-07-13 14:28:33 +0200

    Update remoteOTA.ino

--- examples/remoteOTA/remoteOTA.ino
+++ examples/remoteOTA/remoteOTA.ino
@@ -1,8 +1,12 @@
-
 #include <esp-fs-webserver.h>  // https://github.com/cotestatnt/esp-fs-webserver
 
-#include <HTTPClient.h>
-#include <HTTPUpdate.h>
+#ifdef ESP8266
+  #include <ESP8266HTTPClient.h>
+  #include <ESP8266httpUpdate.h>
+#elif defined(ESP32)
+  #include <HTTPClient.h>
+  #include <HTTPUpdate.h>
+#endif
 #include <EEPROM.h>            // For storing the firmware version
 
 #include <FS.h>
@@ -25,8 +29,7 @@
 uint8_t ledPin = LED_BUILTIN;
 bool apMode = false;
 const char* hostname = "fsbrowser";
-
-String firmware_version;
+char fw_version[10] = {"0.0.0"};
 
 #ifdef ESP8266
   ESP8266WebServer server(80);
@@ -41,7 +44,7 @@ IPAddress startWiFi(){
   Serial.printf("Connecting to %s\n", WiFi.SSID().c_str());
   WiFi.mode(WIFI_STA);
   WiFi.begin();
-  // WiFi.begin(ssid, password);
+  
   uint32_t startTime = millis();
   while (WiFi.status() != WL_CONNECTED ){
     delay(500);
@@ -121,24 +124,42 @@ void handleLed() {
 void handleUpdate() {
   if(myWebServer.webserver->hasArg("fwupdate")) {
     bool value = myWebServer.webserver->arg("fwupdate").toInt();
-    String version = myWebServer.webserver->arg("version");
+    String new_version = myWebServer.webserver->arg("version");
     String url = myWebServer.webserver->arg("url");
     String reply = "Firmware is going to be updated to version ";
-    reply += version;
+    reply += new_version;
     myWebServer.webserver->send(200, "text/plain", reply );
+    
     Serial.println(reply);
     Serial.println(url);
+    
     if( value && WiFi.status() == WL_CONNECTED) {
+
+      #ifdef ESP8266
+      #define UPDATER ESPhttpUpdate
+      // onProgress handling is missing with ESP32 library
+      ESPhttpUpdate.onProgress([](int cur, int total){
+          static uint32_t sendT;
+          if(millis() - sendT > 1000){
+              sendT = millis();
+              Serial.printf("Updating %d of %d bytes...\n", cur, total);
+          }
+      });
+      #elif defined(ESP32)
+      #define UPDATER httpUpdate
+      #endif
+
       WiFiClientSecure client;
       client.setInsecure();
-      httpUpdate.rebootOnUpdate(false);
-      httpUpdate.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-      httpUpdate.setLedPin(LED_BUILTIN, LOW);
-      t_httpUpdate_return ret = httpUpdate.update(client, url, version);
-
+      UPDATER.rebootOnUpdate(false);
+      UPDATER.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+      UPDATER.setLedPin(LED_BUILTIN, LOW);
+      t_httpUpdate_return ret = UPDATER.update(client, url, fw_version);
+      client.stop();
+      
       switch (ret) {
         case HTTP_UPDATE_FAILED:
-          Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+          Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", UPDATER.getLastError(), UPDATER.getLastErrorString().c_str());
           break;
 
         case HTTP_UPDATE_NO_UPDATES:
@@ -146,22 +167,33 @@ void handleUpdate() {
           break;
 
         case HTTP_UPDATE_OK:
-          Serial.println("HTTP_UPDATE_OK");
-          firmware_version = version.c_str();
-          EEPROM.put(0, firmware_version);
-          EEPROM.commit();
+          Serial.println("HTTP_UPDATE_OK"); 
+          strcpy(fw_version, new_version.c_str());
+          EEPROM.put(0, fw_version);
+          EEPROM.commit();     
+          Serial.print("System will be restarted with the new version ");
+          Serial.println(fw_version);   
           delay(1000);
-          ESP.restart();
+          ESP.restart();                                  
           break;
       }
-
     }
   }
 }
 
+void handleVersion() {  
+  EEPROM.get(0, fw_version);
+  Serial.printf("\n\nFirmware version: %s\n", fw_version );
+  if(fw_version != NULL) 
+    myWebServer.webserver->send(200, "text/plain", fw_version);  
+  else
+      myWebServer.webserver->send(200, "text/plain", "0.0.0");
+}
 
+///////////////////////////////////  SETUP  ///////////////////////////////////////
 void setup(){
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   EEPROM.begin(128);
 
   // FILESYSTEM INIT
@@ -170,16 +202,18 @@ void setup(){
   // WiFi INIT
   IPAddress myIP = startWiFi();
 
-  EEPROM.get(0, firmware_version);
-  Serial.printf("\n\nFirmware version: %s\n", firmware_version.c_str() );
-
   // Add custom page handlers to webserver
   myWebServer.addHandler("/led", HTTP_GET, handleLed);
   myWebServer.addHandler("/firmware_update", HTTP_GET, handleUpdate);
+  myWebServer.addHandler("/version", HTTP_GET, handleVersion);
+
+  /*
   // Add handler as lambda function
   myWebServer.addHandler("/version", HTTP_GET, []() {
-    myWebServer.webserver->send(200, "text/plain", firmware_version);
+    if(firmware_version.length())
+      myWebServer.webserver->send(200, "text/plain", firmware_version);
   });
+  */
 
   // Start webserver
   if (myWebServer.begin()) {
@@ -192,7 +226,7 @@ void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-
+///////////////////////////////////  LOOP  ///////////////////////////////////////
 void loop() {
   myWebServer.run();
 
@@ -201,4 +235,5 @@ void loop() {
     MDNS.update();
 	#endif
   }
+
 }
